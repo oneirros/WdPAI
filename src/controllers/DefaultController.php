3316a06eae90240a8c0d60787c5d94007b3fb527
oneirros <kuba.bialon2@gmail.com>
2020-12-18 00:58:40 +0100

    framework of routing

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function index() {
+        die("index method");
+    }
+
+    public function projects() {
+        die("projects method");
+    }
+}
