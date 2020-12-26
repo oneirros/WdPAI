8c9083342692599f4db0d71bd1fcdd4f1156bf99
oneirros <kuba.bialon2@gmail.com>
2020-12-26 18:16:54 +0100

    routing: displaying view of login

--- src/controllers/DefaultController.php
+++ src/controllers/DefaultController.php
@@ -5,10 +5,12 @@ require_once 'AppController.php';
 class DefaultController extends AppController {
 
     public function index() {
-        die("index method");
+
+        $this->render("login");
     }
 
     public function projects() {
-        die("projects method");
+
+        $this->render("projects");
     }
 }
